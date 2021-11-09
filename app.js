12e8abfa9231c9a2a9d3539ece93bb33aff5fcf8
jameswan141 <70609875+jameswan141@users.noreply.github.com>
2021-11-08 20:17:41 -0500

    commit on "main"

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-"use strict"
+'use strict'
 
-const switcher = document.querySelector(".btn");
+const switcher = document.querySelector('.btn');
 
-switcher.addEventListener("click", function() {
-    document.body.classList.toggle("dark-theme")
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('dark-theme')
+
     var className = document.body.className;
     if(className == "light-theme") {
         this.textContent = "Dark";
@@ -12,5 +13,6 @@ switcher.addEventListener("click", function() {
         this.textContent = "Light";
     }
 
-    console.log("current class name: " + className);
+    console.log('current class name: ' + className);
+
 });
